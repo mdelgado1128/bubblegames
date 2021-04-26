8046232b793e1d3aa14c6430ba8a93302011a5b6
Tyler Williams <w.tyler1997@gmail.com>
2021-04-26 11:32:02 -0700

    auth merging

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-<<<<<<< HEAD
 // import SignInCard from './pages/Login'
 //import BGLogo from "./Components/Logo";
 //import Kings from './pages/GPKingsCup';
 //import Quarters from "./pages/GPQuarters";
-=======
->>>>>>> db3fc25d27e2de3ac3d170e5b66c0fce6b0fa8fb
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import GamesButtons from './pages/GamesButtons'
@@ -16,17 +13,9 @@ import BeerPongAcc from './pages/GPBeerPong'
 import QuartersAcc from './pages/GPQuarters'
 import FlipcupAcc from './pages/GPFlipCup'
 import CssBaseline from '@material-ui/core/CssBaseline';
-<<<<<<< HEAD
-//import Grid from '@material-ui/core/Grid';
-import BGHeader from './Components/Header'
-//import SideBar from './Components/SideBar.js'
-import {useHistory} from 'react-router-dom'
-import API from './utils/API'
-=======
 import BGHeader from './Components/Header'
 import SideBar from './Components/SideBar.js'
 import ParticlesBg from 'particles-bg';
->>>>>>> db3fc25d27e2de3ac3d170e5b66c0fce6b0fa8fb
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
--- client/src/pages/GPBeerPong.js
+++ client/src/pages/GPBeerPong.js
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-import React, {useEffect} from 'react';
-=======
 import React, { useEffect }from 'react';
->>>>>>> db3fc25d27e2de3ac3d170e5b66c0fce6b0fa8fb
 import { withStyles } from "@material-ui/core/styles";
 import BGLogo from '../Components/Logo'
 import MuiAccordion from '@material-ui/core/Accordion';
 import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
-<<<<<<< HEAD
-import {useHistory} from 'react-router-dom'
-import API from '../utils/API'
-=======
 import Grid from '@material-ui/core/Grid';
 import { useHistory } from "react-router-dom";
 import API from "../utils/API";
->>>>>>> db3fc25d27e2de3ac3d170e5b66c0fce6b0fa8fb
 const Accordion = withStyles({
   root: {
     border: '1px solid rgba(0, 0, 0, .125)',
